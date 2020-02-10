790037676ed68a9b0de84c510e14c0f78494e65d
Devin Singh <notpublic@gmail.com>
2020-02-09 19:46:44 -0700

    Compeleted part 4 Controller parts

--- Continuum/Controllers/View Controllers/PostDetailTableViewController.swift
+++ Continuum/Controllers/View Controllers/PostDetailTableViewController.swift
@@ -22,6 +22,8 @@ class PostDetailTableViewController: UITableViewController {
     // MARK: - Outlets
     
     @IBOutlet weak var postImageView: UIImageView!
+    @IBOutlet weak var followPostButton: UIButton!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -40,6 +42,14 @@ class PostDetailTableViewController: UITableViewController {
     }
     
     @IBAction func followButtonPressed(_ sender: Any) {
+        guard let post = post else { return }
+          PostController.shared.toggleSubscriptionTo(commentsForPost: post, completion: { (success, error) in
+            if let error = error{
+              print("\(error.localizedDescription) \(error) in function: \(#function)")
+              return
+            }
+            self.updateFollowPostButtonText()
+          })
     }
     
     // MARK: - Functions
@@ -47,7 +57,19 @@ class PostDetailTableViewController: UITableViewController {
     func updateViews() {
         guard let post = post else { return }
         postImageView.image = post.photo
+        updateFollowPostButtonText()
         tableView.reloadData()
+    }
+    
+    func updateFollowPostButtonText(){
+      guard let post = post else { return }
+      //Check CloudKit for a subscription to this post and adjust the text of the button to reflect this
+      PostController.shared.checkForSubscription(to: post) { (found) in
+        DispatchQueue.main.async {
+          let followPostButtonText = found ? "Unfollow Post" : "Follow Post"
+          self.followPostButton.setTitle(followPostButtonText, for: .normal)
+        }
+      }
     }
     
     func presentCommentAlertController() {
